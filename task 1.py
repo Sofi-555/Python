c35a2fb9436b6e8382193aec1caf91e92e7584de
Sofi-555 <sofiagrynchuk@gmail.com>
2023-09-08 22:39:48 +0300

    commit

---
+++ task 1.py
@@ -0,0 +1,17 @@
+a = 1
+b = 1.5
+c = 'Hello World'
+d = True
+e = False
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
