cafbfd53d1bd4fe08fecdf84cc67f698378730e8
Sofi-555 <sofiagrynchuk@gmail.com>
2023-09-20 22:53:12 +0300

    comit

--- Phyton_gm/task1gm.py
+++ Phyton_gm/task1gm.py
@@ -1,80 +1,75 @@
-import random
+
 import time
 
 def start_game():
-    print("Привіт")
-    print("You find yourself at the entrance of a mysterious forest.")
-    print("You can choose your path:")
-    print("1. Take the well-trodden path.")
-    print("2. Venture into the dense undergrowth.")
-
-    choice = input("Enter 1 or 2: ")
+    print("Ласкаво просимо в гру!")
+    time.sleep(1)
+    print("Ви попали у велике місто.Ваш вибір:")
+    time.sleep(1)
+    print("1. Піти з друзями випити кави.\n2.Прогулятись парком.\n")
     
-    if choice == '1':
-        follow_path()
-    elif choice == '2':
-        explore_undergrowth()
+    choice = input("Введіть 1 чи 2: ")
+    
+    if choice == "1":
+        coffe_time()
+    elif choice == "2":
+        park_time()
     else:
-        print("Invalid choice. Please enter 1 or 2.")
+        print("Ваш вибір неможливй.Введіть 1 або 2.")
         start_game()
 
-def follow_path():
-    print("You follow the well-trodden path deeper into the forest.")
-    print("As you walk, you come across a fork in the road.")
-    print("1. Go left.")
-    print("2. Go right.")
+def coffe_time():
+    print("\nВи прийшли в кафе.")
+    print("Яку каву оберете?\n1.Капучіно\n2.Лате\n")
     
-    choice = input("Enter 1 or 2: ")
+    choice = input("Введіть 1 чи 2: ")
     
-    if choice == '1':
-        print("You encounter a friendly fairy who guides you safely out of the forest.")
-        print("Congratulations, you've completed the adventure!")
-    elif choice == '2':
-        print("You stumble upon a hidden treasure chest!")
-        print("You are now rich beyond your wildest dreams.")
-        print("Congratulations, you've completed the adventure!")
+    if choice == "1":
+        print("\nВи насолоджуєтесь своїм капучіно.\nРаптом до вас приходить незнайомець,просить у вас гроші.")
+        print("1.Ви позичаєте пару гривень.\n2.Ви не звертаєте на нього увагу.\n")
+        choice = input("Ваш вибір: ")
+        if choice == "1":
+           print("\nУ вас добра душа.Ви виграли!")
+        elif choice == "2":
+            print("\nВи не добра людина.Ви програли!")
+            
+    elif choice == "2":
+        print("\nВи насолоджуєтесь своїм лате.\nВи допили свою каву та до вас підходить офіціант.\nВи оплачуєте замовлення.\nОплачуючи,ви замічаєте на столі офіцінтки коробку з чайовими.\nЧи дастети ви чайові?\n1.Так\n2.Ні\n")
+        choice = input("Введіть 1 чи 2: ")
+        if choice == "1":
+            print("\nУ вас добра душа.Ви виграли!")
+        elif choice == "2":
+            print("\nВи не добра людина.Ви програли!")
+    
     else:
-        print("Invalid choice. Please enter 1 or 2.")
-        follow_path()
+        print("Ваш вибір неможливй.Введіть 1 або 2.")
+        coffe_time()
 
-def explore_undergrowth():
-    print("You venture into the dense undergrowth, and it starts to get dark.")
-    print("Suddenly, you hear a growl behind you. A wild beast is approaching!")
-    print("1. Try to climb a tree to escape.")
-    print("2. Attempt to confront the beast.")
+def park_time():
+    print("\nВи відправились на прогулянку парком.\nЯкий шлях ви оберете?\n1.Право.\n2.Ліво")
     
-    choice = input("Enter 1 or 2: ")
+    choice = input("Введіть 1 чи 2: ")
     
-    if choice == '1':
-        print("You successfully climb a tree and wait for the beast to pass by.")
-        print("Once it's safe, you climb down and continue your journey.")
-        continue_journey()
-    elif choice == '2':
-        print("You bravely confront the beast, but it's too strong.")
-        print("Unfortunately, you are defeated. Game over.")
-    else:
-        print("Invalid choice. Please enter 1 or 2.")
-        explore_undergrowth()
-
-def continue_journey():
-    print("As you continue your journey, you encounter a talking rabbit.")
-    print("The rabbit offers you a choice:")
-    print("1. Follow the rabbit to a hidden meadow.")
-    print("2. Ignore the rabbit and continue on your path.")
-
-    choice = input("Enter 1 or 2: ")
-
-    if choice == '1':
-        print("You follow the rabbit to a beautiful meadow filled with magical creatures.")
-        print("You've found a place of wonder and tranquility.")
-        print("Congratulations, you've completed the adventure!")
-    elif choice == '2':
-        print("You continue on your path, and the forest becomes even more mysterious.")
-        print("Who knows what adventures lie ahead?")
-        print("Congratulations, you've completed the adventure!")
-    else:
-        print("Invalid choice. Please enter 1 or 2.")
-        continue_journey()
-
-if __name__ == '__main__':
-    start_game()
+    if choice == "1":
+        print("\nВи помічаєте незрячу бабусю,яка наближається до пішохідного переходу\nЧи врятуєте ви бабусю?\n1.Так\n2.Ні")
+        choice = input("Введіть 1 чи 2: ")
+        if choice == "1":
+            print("\nУ вас добра душа.Ви виграли!")
+        elif choice == "2":
+          print("\nВи не добра людина.Ви програли!")
+            
+        
+    elif choice == "2":
+        print("\nВи проходите повз вуличного музиканта,граючого на гітарі.\nВам сподобалась його гра.\nВи помічаєте табличку з надписом 'Збираю гроші'.\nВаш вибір:\n1.Дати гроші\n2.Йти далі\n")
+        choice = input("Введіть 1 чи 2: ")
+        if choice == "1":
+            print("\nУ вас добра душа.Ви виграли!")
+        elif choice == "2":
+            print("\nВи не добра людина.Ви програли!")
+    
+        else:
+          print("Ваш вибір неможливй.Введіть 1 або 2.")
+          park_time()
+          
+if __name__ == "__main__":
+     start_game()
